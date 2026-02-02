27d255ce42772622eeef2b466605383081606b6e
gemarodzwillingscwester <gemarodzwillingscwester>
2026-02-02 18:52:54 +0100

    2feb

---
+++ python25listastuplas.py
@@ -0,0 +1,10 @@
+print("Listas con Python")
+ListasNumeros=[1,5,2]
+for i in range(len(ListasNumeros)):
+    print(ListasNumeros[i])
+tupla=("Leche","Cacao","Avellanas","Azucar")
+tupla[1]="VA"
+print(tupla)
+
+
+
---
+++ python28numeros.py
@@ -0,0 +1,8 @@
+#pedir un numero al usuario
+#mientras que el numero no sea -1, pedir otro numero
+#cuando sea -1, mostramos la suma
+print("dame un numero ")
+numero=input()
+while(numero!=-1):
+    print("dame un numero ")
+    numero=input()
